fac5519ccca2230a3d7069f8cdda235fb2564fc8
Linh <linh44h@gmail.com>
2018-09-08 20:33:10 -0400

    done

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
   // WSENDPOINT: 'ws://localhost:8080',
   HTTPENDPOINT: 'http://theapi.link',
   WSENDPOINT: 'ws://theapi.link/socket',
-  ID: 'YOUR ID HERE' //preassigned group ids
+  ID: 16 //preassigned group ids
 }
 
 config.DEFAULTARRAY = new Uint8ClampedArray(Math.pow(config.DEFAULTLENGTH, 2) * 4).fill('33')
